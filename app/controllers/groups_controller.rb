c1a1f48c4f8ee120872e9625fcf1cad0baf41089
takemoto715 <falloutboy174@gmail.com>
2019-10-07 23:10:27 +0900

    group-model

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,22 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(create_params) 
+    redirect_to root_path
+  end
+  
+  def edit   
+  end
+
+  def update  
+  end
+
+  private
+
+  def create_params
+    params.require(:group).permit(:name)
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   has_many :messages
   has_many :users, through: :groupts_users
   has_many :groups_users
+  validates :name, presence: true, uniqueness: true
 end
