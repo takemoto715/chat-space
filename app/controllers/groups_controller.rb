f486be47991d16ea65774c5843408dab126feec8
takemoto715 <falloutboy174@gmail.com>
2019-10-09 16:00:48 +0900

    group-edit
    add-group-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,22 +1,37 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+  
+  def index
+  end
+  
   def new
     @group = Group.new
+    @group.users << current_user
   end
 
   def create
-    Group.create(create_params) 
-    redirect_to root_path
-  end
-  
-  def edit   
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
-  def update  
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
 
-  def create_params
-    params.require(:group).permit(:name)
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
