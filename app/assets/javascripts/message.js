02409eb1f5d593b30203a3f9c8939d80cd246106
takemoto715 <falloutboy174@gmail.com>
2019-11-02 15:37:44 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function() {
   function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message" data-id="${message.id}">
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="message__upper-info">
                     <p class="message__upper-info__talker">
                       ${message.user_name}
@@ -20,39 +20,33 @@ $(document).on('turbolinks:load', function() {
                 </div>`
   return html;
   }
-  $('#new_message').on('submit', function(e){
+  $("#new_message").on('submit',function(e){
     e.preventDefault();
-    var message = new FormData(this); 
-    var url = $(this).attr('action')
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
-      type: 'POST',
-      data: message,
+      type: "POST",
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('#new_message')[0].reset();
+    .done(function(message){
+      var html = buildHTML(message)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('.messages').append(html)
+      $('form')[0].reset();
     })
-    .fail(function(data){
-      alert('エラーが発生したためメッセージは送信できませんでした。');
+    .fail(function(){
+      alert('messageか画像を入力してください')
     })
-    .always(function(data){
-      $('.submit-btn').prop('disabled', false);
-    })
-    .done(function scrollBottom(){
-      var target = $('.message').last();
-      var position = target.offset().top + $('.messages').scrollTop();
-      $('.messages').animate({
-        scrollTop: position
-      }, 300, 'swing');
-    })
+    return false;
+  })
       var reloadMessages = function() {
         if (window.location.href.match(/\/groups\/\d+\/messages/)){
         last_message_id = $('.message:last').data("message-id"); 
+        console.log(last_message_id);
         $.ajax({
           url: "api/messages",
           type: 'get',
@@ -70,8 +64,8 @@ $(document).on('turbolinks:load', function() {
         .fail(function () {
           alert('自動更新に失敗しました');
         });
-       }
-     };
+       };
+     }
      setInterval(reloadMessages, 5000);
-   });
 });
+
